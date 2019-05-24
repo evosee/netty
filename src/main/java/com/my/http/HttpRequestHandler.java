6b32d96a016ca54566108ba5af8f5b7105cd0822
chensai <chensai123>
2019-05-24 17:24:26 +0800

    commit

---
+++ src/main/java/com/my/http/HttpRequestHandler.java
@@ -0,0 +1,21 @@
+package com.my.http;
+
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.FullHttpRequest;
+
+/**
+ * @author chensai
+ * @version 1.0
+ * @date 2019/5/24 17:14
+ */
+public class HttpRequestHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, FullHttpRequest msg) throws Exception {
+        System.out.println(msg.uri());
+
+        ctx.writeAndFlush("").addListener(ChannelFutureListener.CLOSE);
+    }
+}
---
+++ src/main/java/com/my/http/HttpServer.java
@@ -0,0 +1,50 @@
+package com.my.http;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+/**
+ * @author chensai
+ * @version 1.0
+ * @date 2019/5/24 16:51
+ */
+public class HttpServer {
+    private int port;
+
+    public HttpServer(int port) {
+        this.port = port;
+    }
+
+    public void start() throws InterruptedException {
+        EventLoopGroup eventExecutors = new NioEventLoopGroup();
+
+        try{
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(eventExecutors).localAddress(port).channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<Channel>() {
+                        @Override
+                        protected void initChannel(Channel ch) throws Exception {
+                            ChannelPipeline pipeline = ch.pipeline();
+                            pipeline.addLast(new HttpServerCodec());// http 编解码
+                            pipeline.addLast("httpAggregator", new HttpObjectAggregator(512 * 1024)); // http 消息聚合器                                                                     512*1024为接收的最大contentlength
+                            pipeline.addLast(new HttpRequestHandler());// 请求处理器
+                        }
+                    });
+
+            ChannelFuture future = b.bind().sync();
+            future.channel().closeFuture().sync();
+
+        }finally {
+            eventExecutors.shutdownGracefully().sync();
+        }
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new HttpServer(8888).start();
+    }
+}
